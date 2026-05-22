0233f5139aaf03c25aee7cc77f1a85f359d0b0f2
Swati <its.sbs137@gmail.com>
2026-05-22 21:58:54 +0530

    Add files via upload
    Added initial Netflix analysis project files

---
+++ netflix_analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("netflix_titles.csv")
+
+print(df.head())
+
+print(df.info())
+
+print(df.isnull().sum())
+
+print(df.columns)
+print(df.shape)
+print(df["type"].value_counts())
+
